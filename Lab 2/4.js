0fb1f31f0df69e066a2a029c9f3aed9739dd3f5b
Illya8602 <illyacemchenko@gmail.com>
2024-12-01 20:35:18 +0200

    Create 4.js

---
+++ Lab 2/4.js
@@ -0,0 +1,12 @@
+'use strict';
+const range = (start, end) => {
+    if (start > end) {
+       
+    }
+    let array = [];
+    for (let j = start; j <= end; j++) {
+        array.push(j);
+    }
+    return array;
+}
+console.log(range(15, 30));
