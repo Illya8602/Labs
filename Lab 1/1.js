590a2fb6f2b63ed076d012469f3405f729c44d1b
Illya8602 <illyacemchenko@gmail.com>
2024-12-01 20:31:40 +0200

    Update 1.js

--- Lab 1/1.js
+++ Lab 1/1.js
@@ -1,1 +1,13 @@
+const mixedArray = [ true, 'hello', 5, 12, -200, false, false, 'word' ];
+const typeCount = { 
+    number: 0, 
+    string: 0, 
+    boolean: 0 
+};
 
+for (const item of mixedArray) {
+    const type = typeof item;
+    typeCount[type]+= 1 ;
+}
+
+console.log(typeCount);
