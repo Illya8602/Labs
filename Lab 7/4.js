a5c1f255d04f6de82ae4f73d7656b3dee3fe7bff
Illya8602 <illyacemchenko@gmail.com>
2024-12-01 21:09:55 +0200

    Update 4.js

--- Lab 7/4.js
+++ Lab 7/4.js
@@ -2,13 +2,12 @@ function difference(array1, array2) {
     return array1.filter(item => !array2.includes(item));
 }
 
-// Example usage:
-const array1a = [3, -1, 9, 4, 7];
+const array1a = [7, -2, 10, 5, 0];
 const array2a = [0, 10];
 const result1 = difference(array1a, array2a);
-console.log(result1);
+console.log(result1); 
 
-const array1b = ['Australia', 'Bulgaria'];
-const array2b = ['Brazil', 'Italy', 'Portugal'];
+const array1b = ['Beijing', 'Kiev'];
+const array2b = ['Kiev', 'London', 'Baghdad'];
 const result2 = difference(array1b, array2b);
 console.log(result2); 
