436943d10c97d01f981f7c3fa37933063c6a08b0
Illya8602 <illyacemchenko@gmail.com>
2024-12-01 21:01:38 +0200

    Create 2.js

---
+++ Lab 7/2.js
@@ -0,0 +1,8 @@
+function removeElements(array, ...items) {
+    for (const item of items) {
+        let index;
+        while ((index = array.indexOf(item)) !== -1) {
+            array.splice(index, 1);
+        }
+    }
+}
