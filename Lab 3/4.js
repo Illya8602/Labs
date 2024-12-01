57603a1a0562422dca74de55094072e48c02432c
Illya8602 <illyacemchenko@gmail.com>
2024-12-01 20:44:36 +0200

    Update 4.js

--- Lab 3/4.js
+++ Lab 3/4.js
@@ -1,10 +1,3 @@
-function introspectObject(iface) {
-    return Object.keys(iface) 
-        .filter(key => typeof iface[key] === 'function') 
-        .map(key => [key, iface[key].length]); 
-}
-
-
 const iface = {
     m1: x => [x],
     m2: function (x, y) {
@@ -13,6 +6,13 @@ const iface = {
     m3(x, y, z) {
         return [x, y, z];
     }
+
+function introspectObject(iface) {
+    return Object.keys(iface) 
+        .filter(key => typeof iface[key] === 'function') 
+        .map(key => [key, iface[key].length]); 
+}
 };
+
 
 console.log(introspectObject(iface)); 
